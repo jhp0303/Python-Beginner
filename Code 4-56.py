76b9c47ebd83686ea9b4cc8544bd5fc14a4af64e
Surplus <yingouyingou@gmail.com>
2019-06-04 15:35:21 +0900

    list() 함수의 결과

---
+++ Code 4-56.py
@@ -0,0 +1,9 @@
+# 변수를 선언합니다.
+example_a = "문자열"
+example_b = { "키A":"값A", "키B":"값B" }
+example_c = range(10)
+
+# 출력합니다.
+print("list({}) = {}".format(example_a, list(example_a)))
+print("list({}) = {}".format(example_b, list(example_b)))
+print("list({}) = {}".format(example_c, list(example_c)))
