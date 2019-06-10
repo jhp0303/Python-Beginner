5dfaa6a716694d134e058a403f1bd4b3729ff813
Surplus <yingouyingou@gmail.com>
2019-06-10 12:10:49 +0900

    조건을 활용한 리스트 내포

---
+++ Code 4-64.py
@@ -0,0 +1,7 @@
+# 리스트를 선언합니다.
+array = ["사과", "자두", "초콜릿", "바나나", "체리"]
+output = [fruit for fruit in array if fruit != "초콜릿"]
+# array의 요소를 fruit라고 할 때, 초콜릿이 아닌 fruit로 리스트를 재조합해주세요.
+
+# 출력합니다.
+print(output)
